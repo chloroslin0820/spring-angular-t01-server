835be8b1bcf24f45376f05d214fdeaf828d0b880
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-14 02:18:18 +0800

    add cors

--- src/main/java/com/server/entity/Post.java
+++ src/main/java/com/server/entity/Post.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
