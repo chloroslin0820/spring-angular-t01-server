71fad52f947737ec3701a05ed75834619e46b647
Chloros <chloros@Chloross-MacBook-Pro.local>
2024-07-15 15:39:42 +0800

    fix requestBody content

--- src/main/java/com/server/controller/CommentController.java
+++ src/main/java/com/server/controller/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("comments/create")
-    public ResponseEntity<?> createComment(@RequestParam Long postId, @RequestParam String postedBy, @RequestParam String content) {
+    public ResponseEntity<?> createComment(@RequestParam Long postId, @RequestParam String postedBy, @RequestBody String content) {
         try {
             return ResponseEntity.ok(commentService.createComment(postId, postedBy, content));
         } catch (Exception e) {
